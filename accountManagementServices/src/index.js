ae94d9b90661e2eb8025b79a505223d4a0e4a793
Ayushya <jaiswalayushya@gmail.com>
2023-12-20 20:15:50 +0530

    Initialized app setup

--- accountManagementServices/src/index.js
+++ accountManagementServices/src/index.js
@@ -2,6 +2,7 @@
 
 import dotenv from "dotenv";
 import dbConnection from "./db/index.js";
+import app from "./app.js";
 
 dotenv.config({
     path: './env'
@@ -9,7 +10,10 @@ dotenv.config({
 
 dbConnection()
 .then(() => {
-    // no operation
+    const port = process.env.PORT || 4800;
+    app.listen(port, () => {
+        console.log(`Server started at port: ${port}`);
+    });
 }).catch((err) => {
     console.log('DB Connection failed! ', err);
 });
