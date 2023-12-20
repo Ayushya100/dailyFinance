a3080c8064417c3b6479f97aba6158c2863696c0
Ayushya <jaiswalayushya@gmail.com>
2023-12-20 19:51:51 +0530

    Initialized db connection

---
+++ accountManagementServices/src/constants.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const DB_NAME = 'userCollection';
+
+export {
+    DB_NAME
+};
---
+++ accountManagementServices/src/index.js
@@ -0,0 +1,15 @@
+"use strict";
+
+import dotenv from "dotenv";
+import dbConnection from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+dbConnection()
+.then(() => {
+    // no operation
+}).catch((err) => {
+    console.log('DB Connection failed! ', err);
+});
